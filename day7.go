ec8f965b21a3e5ab715efded76ba19eac8e5a72f
Dominique NORMAND <dominique@normand.us>
2021-01-28 22:17:22 -0500

    day7 done

--- day7.go
+++ day7.go
@@ -8,16 +8,83 @@ import (
 type Day7 struct {
 }
 
+func (d Day7) isABBA(ip string) bool {
+	inside := false
+
+	l := len(ip)
+
+	good := false
+
+	for i, c := range ip {		
+		if i+4 > l { break }
+
+		if c == '[' { 
+			inside = true 
+		} else if c == ']' { 
+			inside = false 
+		} else {
+			if ip[i+1] == ']' || ip[i+1] == '[' { continue }
+			if ip[i] == ip[i+3] && ip[i+1] == ip[i+2] && ip[i] != ip[i+1] {
+				if inside { return false }
+				good = true
+			}
+		}
+	}
+
+	return good
+}
+
+func (d Day7) isABA(ip string) bool {
+	inside := false
+
+	ABA := make(map[string]string)
+	BAB := make(map[string]string)
+
+	l := len(ip)
+
+	for i, c := range ip {		
+		if i+3 > l { break }
+
+		if c == '[' { 
+			inside = true 
+		} else if c == ']' { 
+			inside = false 
+		} else {
+			if ip[i+1] == ']' || ip[i+1] == '[' { continue }
+			if ip[i] == ip[i+2] && ip[i] != ip[i+1] {
+				k1 := fmt.Sprintf("%c%c%c", c, ip[i+1], c)
+				k2 := fmt.Sprintf("%c%c%c", ip[i+1], c, ip[i+1])
+				if inside { 
+					BAB[k1] = k2
+				} else {
+					ABA[k1] = k2
+				}
+			}
+		}
+	}
+
+	for k1, k2 := range ABA {
+		if BAB[k2] == k1 { return true }
+	}
+	return false
+}
+
 func (d Day7) part1() int {
-	// for line := range readlines(7) {
-	// }
-	return 0
+	count := 0
+
+	for line := range readlines(7) {
+		if d.isABBA(line) { count++ }
+	}
+	return count
 }
 
 func (d Day7) part2() int {
-	// for line := range readlines(7) {
-	// }
-	return 0
+	count := 0
+
+	for line := range readlines(7) {
+		if d.isABA(line) { count++ }
+	}
+	return count
 }
 
 func (d Day7) run() {
