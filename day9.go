ad33cb7b51bae72755757b3502b79d8de6c70d48
Dominique NORMAND <dominique@normand.us>
2021-01-29 23:44:50 -0500

    day 9 done

--- day9.go
+++ day9.go
@@ -2,22 +2,58 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"strconv"
 )
 
 // Day9 link https://adventofcode.com/2016/day/9
 type Day9 struct {
 }
 
+func (d Day9) getText() string { 
+	var text strings.Builder
+	for line := range readlines(9) {
+		text.WriteString(line)
+	}
+	return text.String()
+}
+
+func (d Day9) decompress(text string, recursive bool) int {
+	length := len(text)
+
+	count := 0
+	for i := 0; i < length; i++ {
+		if text[i] == '(' {
+			start := i + 1
+			for text[i] != ')' {
+				i++
+			}
+			end := i
+			exp := strings.Split(text[start:end], "x")
+			l, _ := strconv.Atoi(exp[0])
+			r, _ := strconv.Atoi(exp[1])
+
+			if recursive {
+				innerText := text[i+1:i+1+l]
+				l2 := d.decompress(innerText, true)
+				count += l2*r
+			} else {
+				count += l*r
+			}
+			i += l
+		} else {
+			count++
+		}
+	}
+	return count
+}
+
 func (d Day9) part1() int {
-	// for line := range readlines(9) {
-	// }
-	return 0
+	return d.decompress(d.getText(), false)
 }
 
 func (d Day9) part2() int {
-	// for line := range readlines(9) {
-	// }
-	return 0
+	return d.decompress(d.getText(), true)
 }
 
 func (d Day9) run() {
