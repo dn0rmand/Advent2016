df6f05bd35ec89b3bf109fc7598ba0c92b53c9fb
Dominique NORMAND <dominique@normand.us>
2021-02-06 08:29:54 -0500

    day 18 done

--- day18.go
+++ day18.go
@@ -8,16 +8,47 @@ import (
 type Day18 struct {
 }
 
+func (d Day18) countSafeTiles(rows int) int {
+	row := []byte(readline(18))
+	l   := len(row)
+
+	count := 0
+	for _, c := range row { 
+		if c == '.' { count++ }
+	}
+
+	for i := 1; i < rows; i++ {
+		newRow := make([]byte, l)
+	
+		count += l
+		for c := 0; c < l; c++ {
+			newRow[c] = '.'
+			left := byte('.')
+			right:= byte('.')
+			if c > 0 { left = row[c-1] }
+			if c < l-1 { right = row[c+1] }
+
+			if left == '^' && right == '.' { 
+				newRow[c] = '^' 
+				count--
+			} else if left == '.' && right == '^' {
+				newRow[c] = '^'
+				count--
+			} 
+		}
+
+		row = newRow
+	}
+
+	return count
+}
+
 func (d Day18) part1() int {
-	// for line := range readlines(18) {
-	// }
-	return 0
+	return d.countSafeTiles(40)
 }
 
 func (d Day18) part2() int {
-	// for line := range readlines(18) {
-	// }
-	return 0
+	return d.countSafeTiles(400000)
 }
 
 func (d Day18) run() {
